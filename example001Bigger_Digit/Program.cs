f071a56fda7c803c2d454bc6b03531ac353a160d
Oleksii Ishchenko <optionalzirelleariz>
2023-08-13 00:56:41 -0600

    correct version

--- example001Bigger_Digit/Program.cs
+++ example001Bigger_Digit/Program.cs
@@ -1,7 +1,12 @@
-﻿int num = new Random().Next(100, 1000);
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+int num = new Random().Next(100, 1000);
 
 int num1 = num / 100;
 int num2 = num / 10 - num1*10;
 int num3 = num % 10;
 
-Console.WriteLine($"{num} and {num1} and {num3}");
+Console.WriteLine($"{num} and {num2}");
