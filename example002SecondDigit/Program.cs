a2c7b5a684a142096638a5a040137d278c3dd520
Oleksii Ishchenko <optionalzirelleariz>
2023-08-15 08:07:14 -0600

    done without char

--- example002SecondDigit/Program.cs
+++ example002SecondDigit/Program.cs
@@ -2,19 +2,36 @@
 // 645 -> 5
 // 78 -> третьей цифры нет
 // 32679 -> 6
+int num3 = 0;
+string num = Console.ReadLine();
 
-int num = new Random().Next(int.MinValue, int.MaxValue);
-string num_str = num.ToString();
-
-if (num_str.Length >= 3)
+if (num.Length >= 3) // here we have strings, not a value that's why it works
 {
-    char num3_char = num_str[2];
-    int num3 = int.Parse(num3_char.ToString());  
-    Console.WriteLine($"{num} and {num3}");
-}
+    int num_num = Convert.ToInt32(num);
+    if (num_num > 100000)
+    {
+        Console.Write ("choose the shorter number");
+    }
+    if (num_num > 10000)
+    {
+        num3 = num_num / 100 % 10 ;
+        Console.Write($"The third digit is {num3}");
+    }
+    else if (num_num > 1000)
+    {
+        num3 = num_num / 10 % 10;
+        Console.Write($"The third digit is {num3}");
+    }
+    else if (num_num > 100)
+    {
+        num3 = num_num % 10;
+        Console.Write($"The third digit is {num3}");
+    }
+
+    }
+
 else 
 {
     Console.WriteLine($"{num} hasn't the third digit");
 }
-
 
---
+++ exp/Program.cs
@@ -0,0 +1,6 @@
+﻿string a = Console.ReadLine();
+int b = Convert.ToInt32(a);
+int c = b/100;
+
+Console.WriteLine (c);
+Console.WriteLine (c%10);
